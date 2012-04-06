91013854c4cc7d62b960f9f51958552b9e7c24da
Patryk Peszko <comes.de@gmail.com>
2012-04-06 18:25:23 +0200

    displaying mentions

---
+++ BCNTweet/LTAMentionsViewController.h
@@ -0,0 +1,13 @@
+//
+//  LTAMentionsViewController.h
+//  BCNTweet
+//
+//  Created by Patryk Filip Peszko on 4/6/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LTAMentionsViewController : UITableViewController
+
+@end
